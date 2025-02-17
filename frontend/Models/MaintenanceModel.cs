6bdd32957bd9b9ec8a913d50baed996ed6dff74d
Prince-Malaviya <princemalaviya44@gmail.com>
2025-02-17 12:49:46 +0530

    second commit

---
+++ frontend/Models/MaintenanceModel.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SocietyMangement.Models
+{
+    public class MaintenanceModel
+    {
+        public int MaintenanceID { get; set; }
+        public int FlatID { get; set; } 
+
+        public string? FlatNumber { get; set; }
+      
+        public int UserID { get; set; }
+        
+        public string? UserName { get; set; }
+        [Required]
+        public decimal Amount { get; set; }
+        [Required]
+        public DateTime DueDate { get; set; }
+        [Required]
+        public string PaymentStatus { get; set; } = "Pending";
+        public DateTime? PaidDate { get; set; } 
+        public string Notes { get; set; } 
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+    }
+    public class GetFlatnumber
+    {
+        public int FlatID { get; set; }
+
+        public string? FlatNumber { get; set; }
+    }
+
+    public class GetUserNameByFlatID
+    {
+        public int UserID { get; set; }
+        public string? UserName { get; set; }
+    }
+
+}
---
+++ frontend/Models/VisitorModel.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SocietyMangement.Models
+{
+    public class VisitorModel
+    {
+        public int VisitorID { get; set; }
+        public string VisitorName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string WhomToMeet { get; set; }
+        public string FlatType { get; set; }
+        public string FlatNumber { get; set; } 
+        public string VisitPurpose { get; set; }
+        public DateTime EntryTime { get; set; } = DateTime.Now;
+        public DateTime? ExitTime { get; set; }
+        public string? Status { get; set; } = "In";
+    }
+    public class FlatByVisitorModel
+    {
+        public int FlatID { get; set; }
+        public string? FlatNumber { get; set; }
+
+    }
+}
